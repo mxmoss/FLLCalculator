d3646d06f048de062d36cc00b30fd057e169d4d3
Moss Drake <mxmossman@gmail.com>
2017-09-06 13:01:39 -0700

    Two changes
    1) Rearrange the Nav bar so it shows the project name
    2) Start on See More/Less

--- src/FLL2013Calc.js
+++ src/FLL2013Calc.js
@@ -14,18 +14,19 @@ function NavBar(props){
     <nav className="navbar navbar-fixed-top navbar-inverse">
       <div className="container">
         <div className="navbar-header">
-          <CurrentScore curScore={props.curScore} />
           <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span className="sr-only">Toggle navigation</span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-          <a className="navbar-brand" href="#">Project name</a>
+          <a className="navbar-brand" href="#">Nature&apos;s Fury</a>
+          <CurrentScore curScore={props.curScore} />
         </div>
         <div id="navbar" className="collapse navbar-collapse">
           <ul className="nav navbar-nav">
             <li className="active"><a href="#">Home</a></li>
+            <li><a href="#reset">Reset</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
@@ -87,11 +88,15 @@ function ARadioGroup(props){
 }
 
 function FullDescription(props){
-  return  <p>{props.desc}</p>
+    if (props.expanded) {
+  return <p>{props.desc} <a href="#collapse">See Less&lt;&lt;</a></p>;
+    } else {
+  return null;
+   }
 }
 
 function ShortDescription(props){
-  return  <p>Mission:{props.desc}</p>
+  return  <p>Mission:{props.desc}  <a href="#collapse">See More&gt;&gt;</a></p>
 }
 
 function ChallengeItem(props) {
@@ -119,8 +124,8 @@ function ChallengeItem(props) {
           default:  return '';
         }
       })()}
-      <ShortDescription desc={challenge.hint} />
-      <FullDescription desc={challenge.description} />
+      <ShortDescription desc={challenge.hint} showing={challenge.expanded}/>
+      <FullDescription desc={challenge.description} showing={challenge.expanded} />
     </div>
   )
 }
@@ -204,7 +209,6 @@ class FLL2013ChallengeCalc extends Component {
         )
       }
     })
-
   }
 
   render() {
